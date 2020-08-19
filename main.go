fa630d29e857ec2f1b7be7bc9671f7d68bbbb9ad
dfrnks <douglasfrancardoso@gmail.com>
2020-08-19 10:40:43 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth = 600
+	screenHeight = 800
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("Initializing SDL;", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"Gaming in Go Ep. 2",
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		screenWidth,
+		screenHeight,
+		sdl.WINDOW_OPENGL,
+	)
+
+	if err != nil {
+		fmt.Println("Initializing window:", err)
+		return
+	}
+
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+
+	if err != nil {
+		fmt.Println("Initializing renderer:", err)
+		return
+	}
+
+	defer renderer.Destroy()
+
+	plr, err := newPlayer(renderer)
+	if err != nil {
+		fmt.Println("Creating player:", err)
+		return
+	}
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+
+		renderer.SetDrawColor(255, 255, 255, 255)
+		renderer.Clear()
+
+		plr.draw(renderer)
+
+		renderer.Present()
+	}
+}
